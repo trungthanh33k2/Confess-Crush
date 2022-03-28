a11a42d0bc662fe751e9879fc6462709edefdfac
Đỗ Trung Thành <95279598+trungthanh33k2@users.noreply.github.com>
2022-03-28 23:05:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "SendTOCongTuaCutMeo",
+    introTitle: 'MyFakeLove VuLePhuongThao:>>',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    Mà sao toi cứ đi nhầm đường
+    Xin lỗi vì sau đây không phải tiếp lời bài hát... Có những điều tình cờ, dù có nói chưa chắc sẽ tin, nma lỡ thương bé rồi. Bản thân mong muốn hơn nữa nma
+    Đằng sau là gì k dám hy vọng quá nhiều `,
+    btnIntro: 'Thoi xem clip cho vui nghe',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao lại như thế',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <3.... Khúc sau copy nguyên source, Táo gửi bé coi cho vui',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
