a5c41b66630e859ba8a62399101f5a94c560f599
Đỗ Trung Thành <95279598+trungthanh33k2@users.noreply.github.com>
2022-04-01 02:46:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: 'Thoi xem clip cho co thoi chu toi biet may nguoi lam gi quan tam toi dau',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Thích lắm (-__-)',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao lại như thế',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Ok lun <3.... Khúc sau copy nguyên source, Táo gửi bé coi cho vui',
+    messDesc: 'Tối nay 12h, qua đón đi chơi nha.',
+    btnAccept: 'Ok lun <3.... (Nma ngủ như con bò). Khúc sau copy nguyên source, Táo gửi bé coi cho vui',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
